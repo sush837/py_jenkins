6e6a5c93085ef0802bab3a1f9b826c6deebc9772
Manju <sushmitareddy2286@gmail.com>
2025-06-09 15:35:37 +0530

    recent file

--- hello.py
+++ hello.py
@@ -2,17 +2,16 @@ print("Hello, Jenkins Automation!")
 print("Successfully Done!")
 a = 10
 b = 20
-def cal():
-    add = a+b
-    sub = a-b
-    mul = a*b
-    div = a/b
-    addition = add
-    substraction = sub
-    multiplication = mul
-    division = div
-     print(addition)
-     print(substraction)
-     print(multiplication)
-     print(div)
+
+def cal(): 
+    addition = a + b
+    subtraction = a - b
+    multiplication = a * b
+    division = a / b
+    print("Addition:", addition)
+    print("Subtraction:", subtraction)
+    print("Multiplication:", multiplication)
+    print("Division:", division)
+
 cal()
+
