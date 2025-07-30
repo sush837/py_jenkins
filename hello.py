b07d5bc1b9beb354c3eb8b6b6cec256a948dc293
sush837 <sushmitareddy2286@gmail.com>
2025-07-30 15:54:02 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -18,4 +18,12 @@ def str(s):
         str1 = x+str1
     return str1
 print(str('Hello'))
+def rep(y):
+    rep2 = {}
+    for x in y:
+        rep2[x] = rep2.get(x,0) +1
+    result = {k:v for k,v in rep2.items() if v>1}
+    rep3 = 'ijkijkijkijkijkijk'
+print(rep(rep3))
+
 
